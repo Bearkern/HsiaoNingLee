48d79d0c2e1bd388c27fa3c4b2a0ab456b171ef8
Bearkern <haolin1224@gmail.com>
2022-03-28 23:14:53 +0800

    add painter description

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,11 +14,7 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div
-        class="collapse navbar-collapse justify-content-between"
-        id="navbarNav"
-        ref="collapse"
-      >
+      <div class="collapse navbar-collapse justify-content-between" id="navbarNav" ref="collapse">
         <ul class="navbar-nav">
           <li class="nav-item">
             <router-link class="nav-link" to="/paintings" @click="closeNavHam"
@@ -27,15 +23,18 @@
           </li>
         </ul>
         <ul class="navbar-nav">
-          <li class="nav-item">
+          <li class="nav-item m-d-none">
             <router-link class="nav-link" to="/admin" @click="closeNavHam"
               ><i class="bi bi-person-circle fs-3 pe-3"></i>登入</router-link
             >
           </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/collections" @click="closeNavHam"
-              ><i class="bi bi-collection-fill fs-3 pe-3"></i>畫作收藏</router-link
-            >
+          <li class="nav-item collection">
+            <router-link class="nav-link" to="/collections" @click="closeNavHam">
+              <i class="bi bi-collection-fill fs-3 pe-3 position-relative"
+                ><em class="translate-middle badge rounded-pill bg-danger fs-6">3</em>
+              </i>
+              <span>收藏</span>
+            </router-link>
           </li>
         </ul>
       </div>
@@ -52,7 +51,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .index-navbar {
-    z-index: 10;
+.index-navbar {
+  z-index: 10;
+}
+.collection {
+  em {
+    // display: none;
+    position: absolute;
+    top: 0;
+    right: -20px;
   }
+}
+@media (max-width: 768px) {
+  .collection {
+    em {
+      display: block;
+    }
+    span {
+      display: none;
+    }
+  }
+  .m-d-none { display: none; }
+}
 </style>
--- src/views/Painter.vue
+++ src/views/Painter.vue
@@ -1,23 +1,57 @@
 <template>
-  <div class="container p-0">
-    <div class="banner pt-7 px-4 d-flex align-items-center">
-      <img class="painter" src="../assets/images/LeeHsiaoNing.jpeg" alt="Lee Hsiao Ning" />
+  <div class="container banner pt-5 px-4 d-flex align-items-center justify-content-between">
+    <div class="description">
+      <p>
+        畫筆下的「花」脫俗，篩去嬌媚，自展輕柔<br />
+        畫筆下的「景」善美，引光入境，自說故事<br />
+        畫筆下的「物」律動，流轉心情，自顯生命
+      </p>
     </div>
+    <img class="painter" src="../assets/images/LeeHsiaoNing.jpeg" alt="Lee Hsiao Ning" />
+  </div>
+  <div class="container intro">
+    <ul>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
 .banner {
   background-image: url('../assets/images/hydrangea.jpg');
   background-size: cover;
-  background-position: center;
+  background-position: center 70%;
   height: calc(100vh);
 }
-
-img.painter {
-  width: 300px;
-  height: 300px;
+.banner {
+  position: relative;
+}
+.painter {
+  position: absolute;
+  border: 3px solid $white;
+  bottom: 70px;
+  right: 70px;
+  width: 200px;
+  height: 200px;
   border-radius: 50%;
+}
+.description {
+  background: rgba(0, 0, 0, 0.35);
+  padding: 50px;
+  color: $white;
+  writing-mode: vertical-lr;
+  letter-spacing: 5px;
+  p {
+    font-size: 24px;
+  }
+}
+@media (max-width: 768px) {
+  .painter {
+    bottom: 30px;
+    right: 30px;
+    width: 150px;
+    height: 150px;
+  }
 }
 </style>
