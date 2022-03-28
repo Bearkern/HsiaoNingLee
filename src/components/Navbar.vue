685e15309bd8bccaf5be9623a4320a6e675d9a23
Bearkern <haolin1224@gmail.com>
2022-03-28 13:07:27 +0800

    add toggle navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,10 +10,15 @@
         aria-controls="navbarNav"
         aria-expanded="false"
         aria-label="Toggle navigation"
+        @click="toggleNavHam"
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+      <div
+        class="collapse navbar-collapse d-flex justify-content-between"
+        id="navbarNav"
+        ref="collapse"
+      >
         <ul class="navbar-nav">
           <li class="nav-item">
             <router-link class="nav-link" to="/paintings">水彩創作</router-link>
@@ -22,13 +27,13 @@
         <ul class="navbar-nav nav-functions d-inline-block">
           <li class="nav-item">
             <router-link class="nav-link" to="/admin"
-              ><i class="bi bi-person-circle fs-3 px-3"></i
-            >登入</router-link>
+              ><i class="bi bi-person-circle fs-3 px-3"></i>登入</router-link
+            >
           </li>
           <li class="nav-item">
             <router-link class="nav-link" to="/collections"
-              ><i class="bi bi-collection-fill fs-3 me-3"></i
-            >畫作收藏</router-link>
+              ><i class="bi bi-collection-fill fs-3 me-3"></i>畫作收藏</router-link
+            >
           </li>
         </ul>
       </div>
@@ -36,10 +41,29 @@
   </nav>
 </template>
 
+<script>
+import Collapse from 'bootstrap/js/dist/collapse';
+
+export default {
+  methods: {
+    closeNavHam() {
+      this.collapse.hide();
+    },
+    toggleNavHam() {
+      this.collapse.toggle();
+    },
+  },
+  mounted() {
+    this.collapse = new Collapse(this.$refs.collapse, {
+      toggle: false,
+    });
+  },
+};
+</script>
+
 <style lang="scss" scoped>
 .index-navbar {
   width: 100%;
-  max-width: 1296px;
   z-index: 10;
   background-color: $martinique;
 }
